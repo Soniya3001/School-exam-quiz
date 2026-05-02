e1cf21e05571974b28d2b49edd160d697fc68800
Soniya3001 <ankur20226520@gmail.com>
2026-05-02 12:24:40 +0530

    Update +html.tsx

--- frontend/+html.tsx
+++ frontend/+html.tsx
@@ -8,7 +8,7 @@ export default function Root({ children }: PropsWithChildren) {
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <link rel="manifest" href="/manifest.json" />
+        <link rel="manifest" href="manifest.json" >
         <meta name="theme-color" content="#1B4332" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
