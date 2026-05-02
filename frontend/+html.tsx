a8c12d8515e62fc780e56457d6877c7fb70b521c
Soniya3001 <ankur20226520@gmail.com>
2026-05-02 05:34:45 +0530

    Update +html.tsx

--- frontend/+html.tsx
+++ frontend/+html.tsx
@@ -7,15 +7,11 @@ export default function Root({ children }: PropsWithChildren) {
       <head>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#1B4332" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="ExamPlatform" />
         <link rel="apple-touch-icon" href="/assets/images/icon.png" />
         <ScrollViewStyleReset />
